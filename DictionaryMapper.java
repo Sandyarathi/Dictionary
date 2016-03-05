f2fc3133a3ba2879dc9b1a4d2e20778892c73a60
SandyarathiDas <sandyarathi1212@gmail.com>
2016-03-04 22:17:03 -0800

    Fixed parts of Speech Errors

--- DictionaryMapper.java
+++ DictionaryMapper.java
@@ -1,6 +1,7 @@
 package Dictionary;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -16,16 +17,24 @@ public class DictionaryMapper  extends Mapper<Text, Text, Text, Text> {
             language = fileName.substring(0,fileName.length()-4);
     }
     public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-            if(!(key.toString().charAt(0)=='#')){
-                    String[] splitValue=value.toString().split("\\[",2);
-                    if(splitValue.length!=0 && splitValue.length!=1){
-                            String keyMap= key + " : ["+splitValue[1].substring(0,splitValue[1].length()-1)+"]";
+            if(!(key.toString().charAt(0)=='#') && (value.toString().indexOf('['))>0){
+                    String[] splitValue=value.toString().split("\\[");
+                    if(splitValue.length!=0 && splitValue.length!=1 ){
+                        String partsOfSpeech = splitValue[1].substring(0,splitValue[1].length()-1);
+                        if(valid(partsOfSpeech)){
+                            String keyMap= key + " : ["+partsOfSpeech+"]";
                             String valueMap = language+ ":"+splitValue[0];
-                context.write(new Text(keyMap), new Text(valueMap));
-            }
+                            context.write(new Text(keyMap), new Text(valueMap));
+                        }
+                        
+                    }
             
-        }
+            }
         
+    }
+    private boolean valid(String partsOfSpeech) {
+        String[] words = {"Noun", "Pronoun", "Verb", "Adverb", "Adjective", "Preposition", "Article", "Conjunction"};  
+        return (Arrays.asList(words).contains(partsOfSpeech));
     }
     
 }
